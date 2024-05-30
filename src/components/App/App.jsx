803a4c9b4599f362e00fd76177c958d582542bab
Sam Shacter <sshacter@gmail.com>
2024-05-30 14:02:51 -0500

    GET and POST routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ShoppingForm from '../ShoppingForm/ShoppingForm.jsx';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+const [items, setItems] = useState([])
+
+useEffect(() => {
+    getItems()
+}, [])
+
+const getItems = () => {
+axios.get('/api/shoppinglist')
+.then(response => {
+    setItems(response.data)
+    console.log("Items", response.data)
+})
+.catch(err => {
+    console.error("error getting list of items", err)
+})
+
+}
+
+
+const addItem = (newItem) => {
+    axios.post('/api/shoppinglist', newItem)
+      .then(() => {
+        getItems()
+      })
+      .catch(err => {
+        console.error('Error adding item:', err);
+      });
+  };
+
+
+
     return (
         <div className="App">
             <Header />
             <ShoppingForm />
+
             <main>
                 <p>Under Construction....</p>
             </main>
