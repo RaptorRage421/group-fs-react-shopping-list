3a758019a6376f63712e88533d14b195565203ff
Sam Shacter <sshacter@gmail.com>
2024-05-30 11:16:11 -0500

    made router page as a group

---
+++ server/shoppingRouter.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const pool = require('./modules/pool')
+
+const router = express.Router()
+
+router.get('/', (req,res) => {
+    const queryText = `
+    SELECT * FROM "shoppinglist"
+        ORDER BY "name" DESC ;
+    `
+
+    pool.query(queryText)
+    .then((results) => {
+        res.send(results.rows)
+    })
+    .catch((err)=> {
+        console.error('Error in GET for /api/shoppinglist', err)
+        res.sendStatus(500)
+    })
+
+})
+
+router.post('/', (req,res) => {
+    const queryText = `
+    INSERT INTO "shoppinglist"
+        ("name", "quantity", "unit")
+        VALUES
+        ($1,$2,$3);
+    `
+    let item = req.body
+    pool.query(queryText, [item.name, item.quantity, item.unit])
+    .then((results) => {
+        res.sendStatus(201)
+    })
+    .catch((err) => {
+        console.error("error in post for /api/shoppinglist", err)
+    
+    })
+
+})
+
+router.delete('/:id', (req,res) =>{
+    const queryText = `
+    DELETE FROM "shoppinglist"
+    WHERE id = $1;
+    `
+    pool.query(queryText, [req.params.id])
+        .then((results) => {
+            res.sendStatus(204)
+        })
+        .catch((err) => {
+            console.error('Issue with the DELETE', err)
+        })
+
+})
